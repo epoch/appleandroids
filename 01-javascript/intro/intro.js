696269646e360c3cde603e8dc6e9fd473ca2ae56
Daniel Tsui <tsui.daniel@gmail.com>
2015-09-28 15:19:43 +1000

    history being created

---
+++ 01-javascript/intro/intro.js
@@ -0,0 +1,10 @@
+console.log('first program its working!!');
+
+var numberOfChildren = 9;
+var partnerName = 'Donald';
+var geographicLocation = 'Sunshine';
+var jobTitle = 'pet food taster';
+
+var fortune = 'You will be a ' + jobTitle + ' in ' + geographicLocation + ', and married to ' + partnerName + ' with ' + numberOfChildren + ' children';
+
+console.log(fortune);
