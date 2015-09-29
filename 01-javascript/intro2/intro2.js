705b8f6e5806d2429560990b61a2a3f0090eb89f
Daniel Tsui <tsui.daniel@gmail.com>
2015-09-29 10:44:49 +1000

    switched on

---
+++ 01-javascript/intro2/intro2.js
@@ -0,0 +1,35 @@
+console.log('its working');
+
+if (5 > 4) {
+  console.log('maths is too easy!');
+}
+
+// values that are "falsey"
+// false, the empty string "", the number 0, undefined. null
+
+var appleAndroidRule = true;
+
+if (appleAndroidRule) {
+  console.log('yay apple andriod');
+}
+
+var switchedOn = true;
+if (switchedOn) {
+  console.log('party till the end');
+}
+
+var myName = '';
+if (myName) {
+  console.log('hello ' + myName);
+}
+
+var points = 0;
+if (points) {
+  console.log('you have ' + points + ' points');
+}
+
+var firstname;
+if (firstname) {
+  console.log('your name is ' + firstname);
+}
+
