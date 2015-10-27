a6b7672926d63e4afac00e16600d12801c9fb604
Daniel Tsui <tsui.daniel@gmail.com>
2015-10-27 12:20:38 +1100

    basic crud for good food hunting

--- 05-database/good_food_hunting/main.rb
+++ 05-database/good_food_hunting/main.rb
@@ -11,13 +11,53 @@ get '/' do
   erb :index
 end
 
+# getting the form
+get '/dishes/new' do
+  erb :new
+end
+
 # single dish show path
 get '/dishes/:id' do
-  db = PG.connect(dbname: 'goodfoodhunting')
-  @dishes = db.exec("select * from dishes where id = #{ params[:id] }") # [{}]
-  db.close
-
+  @dishes = run_sql("select * from dishes where id = #{ params[:id] }") # [{}]
   @dish = @dishes[0] # {}
 
   erb :show
 end
+
+# create a new dish
+post '/dishes' do
+  sql = "insert into dishes (name, image_url) values ('#{ params[:name] }', '#{ params[:image_url] }');"
+  run_sql(sql)
+
+  redirect to '/'
+end
+
+# get edit form for an existing dish
+get '/dishes/:id/edit' do
+  @dishes = run_sql("select * from dishes where id = #{ params[:id] }")
+  @dish = @dishes[0]
+  erb :edit
+end
+
+# update existing dish with specified id
+post '/dishes/:id' do
+  run_sql("UPDATE dishes SET name='#{params[:name]}', image_url='#{params[:image_url]}' where id = #{params[:id]}")
+
+  redirect to "/dishes/#{params[:id]}"
+end
+
+post '/dishes/:id/delete' do
+  run_sql("DELETE FROM dishes where id = #{ params[:id] }")
+
+  redirect to "/"  
+end
+
+
+def run_sql(sql)
+  db = PG.connect(dbname: 'goodfoodhunting')
+  results = db.exec(sql)
+  db.close
+  results
+end
+
+
