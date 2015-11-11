1c1dfd81438c6fa63ce4b27cad445076de864653
Daniel Tsui <tsui.daniel@gmail.com>
2015-11-11 12:28:15 +1100

    for ivan

--- 07-rails/goodfoodhunting/app/models/dish.rb
+++ 07-rails/goodfoodhunting/app/models/dish.rb
@@ -1,6 +1,7 @@
 class Dish < ActiveRecord::Base
   belongs_to :dish_type
-
+  has_many :likes
+  
   validates :name, presence: true
   validates :image_url, length: { minimum: 5 } 
 end
--- 07-rails/goodfoodhunting/db/schema.rb
+++ 07-rails/goodfoodhunting/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110013824) do
+ActiveRecord::Schema.define(version: 20151111012257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20151110013824) do
   end
 
   add_index "dishes", ["dish_type_id"], name: "index_dishes_on_dish_type_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "dish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
